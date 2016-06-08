dd0c6f16483802e6dc7bfcc027a7f153355741bc
Kahototo <johndoe@example.k.t.07242@gmail.com>
2016-06-08 17:45:36 +0900

    Nabeatsu

---
+++ nabeatsu/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  nabeatsu
+//
+//  Created by ohtatomotaka on 2015/03/11.
+//  Copyright (c) 2015年 ohtatomotaka. All rights reserved.
+//
+
+import UIKit
+//TODO:コーディング規約を確認
+
+class ViewController: UIViewController {
+    var number:Int = 0
+    @IBOutlet var label:UILabel!
+    @IBOutlet var faceLabel:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "\(0)"
+    }
+    func isAho() ->Bool{
+        //問題1: 3の倍数かどうか調べる
+        if number % 3 == 0{
+            return true
+        }
+        
+        //問題2: 1の位が３かどうか調べる
+        if number % 10 == 3 {
+            return true //アホになる
+        }
+        
+        
+        //問題3: 10の位が３かどうか調べる
+        if number / 10 % 10 == 3 {
+            return true //アホになる
+        }
+        
+        
+        //問題4: 数値に3がつくか調べる
+        var checkNum : Int
+        checkNum = number //3がつくか新しく整数を定義する
+        while checkNum != 0 {
+            if checkNum % 10 == 3 {
+                return true //アホになる
+            } else {
+                checkNum = checkNum / 10
+            }
+            
+        }
+        //問題4をやるときは問題3の答えをコメントアウトしてから書こう
+        
+        
+        return false
+    }
+    @IBAction func plusButton(){
+        number = number + 1
+        label.text = "\(number)"
+        if isAho() == true {
+            faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
+        } else {
+            faceLabel.text = "(゜o゜)"
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+
+
+/*
+Q1
+if number % 3 == 0{
+return true
+}
+
+Q2
+if number % 10 == 3{
+return true
+}
+
+Q3
+if number / 10 % 10 == 3 {//10で割った後、10で割った余りを出す
+return true
+}
+
+Q4
+while number != 0 {
+if number % 10 == 3 {
+return true
+}else {
+number = number / 10
+}
+}
+
+
+*/
